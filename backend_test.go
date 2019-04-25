500e7cd3da61167cefa3bfdc42ec65816e2a9210
kpacha <kpacha666@gmail.com>
2019-04-25 13:30:07 +0200

    test coverage improved

--- backend_test.go
+++ backend_test.go
@@ -15,6 +15,55 @@ import (
 	"github.com/devopsfaith/krakend/proxy"
 )
 
+func TestBackendFactoryWithInvoker_fallback(t *testing.T) {
+	for i, tc := range []struct {
+		Name string
+		Cfg  *config.Backend
+	}{
+		{
+			Name: "no extra",
+			Cfg: &config.Backend{
+				Method: "GET",
+			},
+		},
+		{
+			Name: "wrong extra type",
+			Cfg: &config.Backend{
+				Method: "GET",
+				ExtraConfig: config.ExtraConfig{
+					Namespace: 42,
+				},
+			},
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			hits := 0
+
+			bf := BackendFactoryWithInvoker(
+				func(remote *config.Backend) proxy.Proxy {
+					hits++
+					return proxy.NoopProxy
+				},
+				invoker(func(in *lambda.InvokeInput) (*lambda.InvokeOutput, error) {
+					t.Error("this invoker should not been called")
+					return nil, nil
+				}),
+			)
+
+			resp, err := bf(tc.Cfg)(context.Background(), &proxy.Request{})
+			if err != nil {
+				t.Error(i, err)
+			}
+			if resp != nil {
+				t.Errorf("%d: unexpected response", i)
+			}
+			if hits != 1 {
+				t.Errorf("unexpected number of hits to the fallback backend factory: %d", hits)
+			}
+		})
+	}
+}
+
 func TestBackendFactoryWithInvoker(t *testing.T) {
 	explosiveBF := func(remote *config.Backend) proxy.Proxy {
 		t.Error("this backend factory should not been called")
@@ -151,6 +200,156 @@ func TestBackendFactoryWithInvoker(t *testing.T) {
 				t.Errorf("unexpected response: %v", resp.Data)
 			}
 		})
+	}
+}
+
+func TestBackendFactoryWithInvoker_error(t *testing.T) {
+	explosiveBF := func(remote *config.Backend) proxy.Proxy {
+		t.Error("this backend factory should not been called")
+		return proxy.NoopProxy
+	}
+
+	bf := BackendFactoryWithInvoker(
+		explosiveBF,
+		invoker(func(in *lambda.InvokeInput) (*lambda.InvokeOutput, error) {
+			if *in.InvocationType != "RequestResponse" {
+				t.Errorf("unexpected InvocationType: %s", *in.InvocationType)
+			}
+			if *in.ClientContext != "KrakenD" {
+				t.Errorf("unexpected ClientContext: %s", *in.ClientContext)
+			}
+			if *in.FunctionName != "python37" {
+				t.Errorf("unexpected FunctionName: %s", *in.FunctionName)
+			}
+			payload := map[string]string{}
+			if err := json.Unmarshal(in.Payload, &payload); err != nil {
+				return nil, err
+			}
+			if payload["first_name"] != "" {
+				t.Errorf("first_name present in the payload request")
+			}
+			if payload["last_name"] != "" {
+				t.Errorf("last_name present in the payload request")
+			}
+			return &lambda.InvokeOutput{
+				Payload:    []byte(`{"message":"Hello  !"}`),
+				StatusCode: aws.Int64(200),
+			}, nil
+		}),
+	)
+
+	r := &proxy.Request{
+		Params: map[string]string{"function": "python37"},
+		Body:   ioutil.NopCloser(bytes.NewBufferString(`"first_name":"foobar","last_name":"some"}`)),
+	}
+	remote := &config.Backend{
+		Method: "POST",
+		ExtraConfig: config.ExtraConfig{
+			Namespace: map[string]interface{}{},
+		},
+	}
+	resp, err := bf(remote)(context.Background(), r)
+	if err == nil {
+		t.Error("error expected")
+		return
+	}
+	if resp != nil {
+		t.Errorf("unexpected response: %v", resp)
+		return
+	}
+}
+
+func TestBackendFactoryWithInvoker_incomplete(t *testing.T) {
+	explosiveBF := func(remote *config.Backend) proxy.Proxy {
+		t.Error("this backend factory should not been called")
+		return proxy.NoopProxy
+	}
+
+	bf := BackendFactoryWithInvoker(
+		explosiveBF,
+		invoker(func(in *lambda.InvokeInput) (*lambda.InvokeOutput, error) {
+			if *in.InvocationType != "RequestResponse" {
+				t.Errorf("unexpected InvocationType: %s", *in.InvocationType)
+			}
+			if *in.ClientContext != "KrakenD" {
+				t.Errorf("unexpected ClientContext: %s", *in.ClientContext)
+			}
+			if *in.FunctionName != "" {
+				t.Errorf("unexpected FunctionName: %s", *in.FunctionName)
+			}
+			payload := map[string]string{}
+			if err := json.Unmarshal(in.Payload, &payload); err != nil {
+				t.Error(err)
+				return nil, err
+			}
+			if payload["first_name"] != "" {
+				t.Errorf("first_name present in the payload request")
+			}
+			if payload["last_name"] != "" {
+				t.Errorf("last_name present in the payload request")
+			}
+			return &lambda.InvokeOutput{
+				Payload:    []byte(`{"message":"Hello  !"}`),
+				StatusCode: aws.Int64(200),
+			}, nil
+		}),
+	)
+
+	remote := &config.Backend{
+		Method: "GET",
+		ExtraConfig: config.ExtraConfig{
+			Namespace: map[string]interface{}{},
+		},
+	}
+	resp, err := bf(remote)(context.Background(), &proxy.Request{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if resp == nil {
+		t.Errorf("unexpected response: %v", resp)
+		return
+	}
+	if !resp.IsComplete {
+		t.Error("incomplete response")
+		return
+	}
+	if m, ok := resp.Data["message"]; !ok || m != "Hello  !" {
+		t.Errorf("unexpected response: %v", resp.Data)
+		return
+	}
+}
+
+func TestBackendFactoryWithInvoker_wrongStatusCode(t *testing.T) {
+	explosiveBF := func(remote *config.Backend) proxy.Proxy {
+		t.Error("this backend factory should not been called")
+		return proxy.NoopProxy
+	}
+
+	bf := BackendFactoryWithInvoker(
+		explosiveBF,
+		invoker(func(in *lambda.InvokeInput) (*lambda.InvokeOutput, error) {
+			return &lambda.InvokeOutput{
+				Payload:    []byte(``),
+				StatusCode: aws.Int64(404),
+			}, nil
+		}),
+	)
+
+	remote := &config.Backend{
+		Method: "GET",
+		ExtraConfig: config.ExtraConfig{
+			Namespace: map[string]interface{}{},
+		},
+	}
+	resp, err := bf(remote)(context.Background(), &proxy.Request{})
+	if err != errBadStatusCode {
+		t.Error(err)
+		return
+	}
+	if resp != nil {
+		t.Errorf("unexpected response: %v", resp)
+		return
 	}
 }
 
